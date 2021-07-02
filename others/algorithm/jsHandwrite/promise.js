4eb9e67a014231deed040e4f9d9e94f573953eb4
dinoZhao <dino.zhao@outlook.com>
2021-07-02 12:02:20 +0800

    fix

--- others/algorithm/jsHandwrite/promise.js
+++ others/algorithm/jsHandwrite/promise.js
@@ -312,3 +312,13 @@ function resolvePromise(promise, x, resolve, reject) {
     resolve(x)
   }
 }
+MyPromise.deferred = function () {
+  var result = {}
+  result.promise = new MyPromise(function (resolve, reject) {
+    result.resolve = resolve
+    result.reject = reject
+  })
+
+  return result
+}
+module.exports = MyPromise
