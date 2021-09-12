0268229897898bad609f7334cc244878aec0c7d0
dino <dino.zhao@outlook.com>
2021-09-12 21:14:35 +0800

    修改hmr

--- others/hmr/server/index.js
+++ others/hmr/server/index.js
@@ -1,20 +1,21 @@
-const express = require("express");
-const webpack = require("webpack");
-const webpackconfig = require("../webpack.config");
-const webpackMiddleware = require("webpack-dev-middleware");
+const express = require('express')
+const webpack = require('webpack')
+const webpackconfig = require('../webpack.config')
+const webpackMiddleware = require('webpack-dev-middleware')
 
-var webpackHotMiddleware = require("webpack-hot-middleware");
+var webpackHotMiddleware = require('webpack-hot-middleware')
 
-const app = express();
+const app = express()
 
-const webpackCompiler = webpack(webpackconfig);
-const wpmw = webpackMiddleware(webpackCompiler, {});
-app.use(wpmw);
+const webpackCompiler = webpack(webpackconfig)
+const wpmw = webpackMiddleware(webpackCompiler, {})
+app.use(express.static('dist'))
+app.use(wpmw)
 app.use(
   webpackHotMiddleware(webpackCompiler, {
     log: console.log,
   })
-);
+)
 app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
-});
+  console.log('Example app listening on port 3000!')
+})
--- others/hmr/webpack.config.js
+++ others/hmr/webpack.config.js
@@ -1,24 +1,18 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
-const dist = path.resolve(__dirname, "www");
+const path = require('path')
+const webpack = require('webpack')
+const dist = path.resolve(__dirname, 'www')
 
 module.exports = {
   entry: [
-    "./client/index.js",
-    "webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true",
+    './client/index.js',
+    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true',
   ],
-  devtool: "inline-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Hot Module Reload",
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
+  devtool: 'inline-source-map',
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   //   target: "web",
-  mode: "development",
+  mode: 'development',
   output: {
-    filename: "bundle.js",
+    filename: 'bundle.js',
     path: path.resolve(__dirname, dist),
   },
-};
+}
